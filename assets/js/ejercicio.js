74f9c3e54017b5baa8964c59d54a8c126eee9806
Polo <mphowitzer@gmail.com>
2023-01-17 21:33:15 -0600

    corte de clase martes 17

---
+++ assets/js/ejercicio.js
@@ -0,0 +1,16 @@
+let carName = "Volvo";
+console.log('carName: ', carName);
+
+
+let x = 50;
+console.log('x: ', x);
+
+let a = 5;
+let b = 10;
+let suma = (a + b);
+console.log('suma: ', suma);
+
+
+let z = "x + y";
+alert("resultado es: " + z)
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,27 @@ var edad = 30;
 const fechaNacimiento = "17/08/1993"; // no se puede reasignar (no debería)
 
 let apellido ="Perez"; // se puede reasignar
-
 console.log('apellido');
+
+let nombreCompleto = "Leopoldo Ramírez";    // String (texto)
+let telefono = 123;     // Number (número)
+let humano = true;  //Boolean (true/false)
+let direccion; // undefined (Que no se le ha asignado un valor)
+let codigoPostal = null; //null que no tiene un valor
+
+
+/*
+console.log("nombreCompleto", typeof nombreCompleto);
+console.log('telefono: ', typeof telefono);
+console.log('humano: ', typeof humano);
+console.log('direccion: ', typeof direccion);
+console.log('codigoPostal: ', typeof codigoPostal);
+*/
+nombreCompleto = String(123);
+telefono = Number("985246");
+console.log("nombreCompleto",  nombreCompleto);
+console.log('telefono: ',  telefono);
+console.log('humano: ',  humano);
+console.log('direccion: ',  direccion);
+console.log('codigoPostal: ',  codigoPostal);
 
