4e88a5c60e641303c56dcb6721c0d9c5c8a81cc8
Polo <mphowitzer@gmail.com>
2023-01-17 20:39:20 -0600

    tipos de datos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
+"use strict";
+
 console.log(`Hola mundo`);
 console.log("Hola mundo");
 console.log('Hola mundo');
+
+var nombre = "Juan";    // se puede reasignar pero no se recomienda su uso ya que su alcance es global.
+var edad = 30;
+
+const fechaNacimiento = "17/08/1993"; // no se puede reasignar (no debería)
+
+let apellido ="Perez"; // se puede reasignar
+
+console.log('apellido');
+
